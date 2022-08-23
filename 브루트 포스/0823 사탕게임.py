d9f129d56211d611c74322ac1b0941ee29669be3
windy82581 <windy82581@gmail.com>
2022-08-23 23:56:57 +0900

    0823

--- 브루트 포스/0823 사탕게임.py
+++ 브루트 포스/0823 사탕게임.py
@@ -6,11 +6,45 @@
 
 
 # 2. 자리바꾼 상태에서 다 체크해보기
-
+def maxChange(case):
+    global answer, n
+    
+    for i in range(n):
+        temp = 1
+        for j in range(1, n):
+            if case[i][j] == case[i][j-1]:
+                temp += 1
+            else:
+                if temp > answer:
+                    answer = temp
+                temp = 1
+        if temp > answer:
+            answer = temp
+    
 
 
 
 # 1. 사탕 자리 바꾸기
 n = int(input())
-case = [input() for _ in range(n)]
-print(case)
+case = [list(input()) for _ in range(n)]
+
+answer = 1
+for i in range(0, n):
+    for j in range(0, n-1):
+        case[i][j], case[i][j+1] = case[i][j+1], case[i][j]
+        maxChange(case)
+        maxChange(list(zip(*case)))
+        case[i][j], case[i][j+1] = case[i][j+1], case[i][j]
+        if answer == n:
+            print(n)
+            quit()
+
+        case[j][i], case[j+1][i] = case[j+1][i], case[j][i]
+        maxChange(case)
+        maxChange(list(zip(*case)))
+        case[j][i], case[j+1][i] = case[j+1][i], case[j][i]
+        if answer == n:
+            print(n)
+            quit()
+
+print(answer)
