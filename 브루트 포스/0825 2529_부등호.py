1df102210b565844bfdc1faea021430b41644ab2
windy82581 <windy82581@gmail.com>
2022-09-17 23:25:15 +0900

    0917

--- 브루트 포스/0825 2529_부등호.py
+++ 브루트 포스/0825 2529_부등호.py
@@ -1,13 +1,38 @@
+from itertools import permutations
+
 k = int(input())
 line = input().split()
 
+per_cases = list(permutations(range(0, 10), k+1))
+min, max = 0, 0
 
-maxx, minn = 0, 0
-target = list(range(10))
-target_reversed = target[::-1]
+# min 구하기
+flag = 0
+for case in per_cases:
+    if flag:
+        break
+    for num_idx in range(1, k+1):
+        if line[num_idx-1] == '<' and case[num_idx-1] >= case[num_idx]:
+            break
+        elif line[num_idx-1] == '>' and case[num_idx-1] <= case[num_idx]:
+            break
+    else:
+        min = ''.join(map(str, case))
+        flag = 1
 
-case = [0] * (k+1)
+# max 구하기
+flag = 0
+for case_idx in range(len(per_cases)-1, -1, -1):
+    case = per_cases[case_idx]
+    if flag:
+        break
+    for num_idx in range(1, k+1):
+        if line[num_idx-1] == '<' and case[num_idx-1] >= case[num_idx]:
+            break
+        elif line[num_idx-1] == '>' and case[num_idx-1] <= case[num_idx]:
+            break
+    else:
+        max = ''.join(map(str, case))
+        flag = 1
 
-visit = [0] * 10
-for num in range(k+1):
-    
+print(max, min, sep='\n')
