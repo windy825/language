7855ebc01f12823b32b0707770518e586b18d746
windy82581 <windy82581@gmail.com>
2022-10-17 22:42:43 +0900

    main() 구조

--- cpp/myFirstHelloWorld/myFirstHelloWorld/Source.cpp
+++ cpp/myFirstHelloWorld/myFirstHelloWorld/Source.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	std::cout << "Hello, World!" << std::endl;
 	// :: namespace, 불러오는 라이브러리 같은 것들은 :: 안에 포함시킨다.
-
+	main2()
 
 	return 0;
 }
--- cpp/myFirstHelloWorld/myFirstHelloWorld/Source1.cpp
+++ cpp/myFirstHelloWorld/myFirstHelloWorld/Source1.cpp
@@ -1,4 +1,21 @@
-int main()
+#include <iostream>  
+// # : 전처리기 preprocessor directive
+// io : input,output
+
+int main(void) // function의 특징, 인자를 받을 소괄호가 있음
 {
-	return 0;
+	int x; // 정수형 데이터를 받을 메모리 공간 x 선언
+	x = (1 + 2) * (3 + 4); // expression
+
+	int y = x + 3;
+
+
+	std::cout << y << std::endl;
+	// std 라이브러리 이름공간에서 cout 사용
+	std::cout << 1 + 2 << std::endl;
+
+	return 0; // statement 명령문, 문장의 끝을 표현하는 ; 가 필요
 }
+
+// main 을 실행시키는건 os임, 이 코드를 os님이 메모리를 사용해서 이렇게
+// 실행시켜 주세요 의미
